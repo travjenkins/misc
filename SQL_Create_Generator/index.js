0330de0bf1b545c39f5dfa013eb85b8541b4b9f1
Travis Jenkins <jamminandprogrammin@gmail.com>
2023-11-01 16:22:42 -0400

    Update index.js
    Added in a UUID generator to reduce chance to name clashes. Still does not guarantee is but better than before.

--- SQL_Create_Generator/index.js
+++ SQL_Create_Generator/index.js
@@ -1,22 +1,22 @@
 const fs = require("fs");
 const dockerNames = require('docker-names');
+const {randomUUID} = require('node:crypto');
 
-const count = 2000;
+const createStream = fs.createWriteStream("kube_name_random_2k.sql");
 
-// Make a random int
 function randomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Generate the file name and stream
-const createStream = fs.createWriteStream(`kube_name_random_${count}.sql`);
-
-// Loop through and make the name AND write to the file
+const count = 2000;
 for (let i = 0; i < count; i += 1) {
     const name = [];
     name.push(dockerNames.getRandomName());
+
+    name.push(randomUUID().split('-')[0]);
+
     name.push(Math.floor(1 + Math.random() * (100 - 1)))
-    name.push(Math.floor(1 + Math.random() * (100 - 1)))
+
     const joinedName = name.join('_');
     createStream.write(`CREATE TABLE ${joinedName} (id SERIAL PRIMARY KEY, test_table_col_a_0 varchar(1), test_table_col_b_0 varchar(5));`);
     createStream.write('\r\n')
